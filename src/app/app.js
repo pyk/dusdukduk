80178cc52affb3fa30f94ea3402efd92a77f8809
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-09-05 19:13:17 +0700

    initialize angular apps

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,14 @@
-var app = angular.module("dusdukduk", []);
+var app = angular.module("dusdukduk", ["ngRoute", "angular-loading-bar"]);
+
+app.config(["$routeProvider", "$locationProvider",
+    function($rp, $lp) {
+    $rp
+    .when("/", {
+        templateUrl: "/assets/views/list_of_products.html",
+        controller: "ListOfProductsCtrl"
+    })
+    .otherwise({
+        redirectTo: "/"
+    });
+
+}]);
---
+++ src/app/controller/ListOfProducts.controller.js
@@ -0,0 +1,4 @@
+app.controller("ListOfProductsCtrl", ["$scope",
+    function($s) {
+
+}]);

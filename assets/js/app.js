28b32f4b59065c6a38c18538567587afbbb84cb3
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-09-05 18:59:47 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module("dusdukduk", []);
+var app = angular.module("dusdukduk", ["ngRoute", "angular-loading-bar"]);
+
+app.config(["$routeProvider", "$locationProvider",
+    function($rp, $lp) {
+    $rp
+    .when("/", {
+        templateUrl: "/assets/views/list_of_products.html",
+        controller: "ListOfProductsCtrl"
+    })
+    .otherwise({
+        redirectTo: "/"
+    });
+
+}]);
+app.controller("ListOfProductsCtrl", ["$scope",
+    function($s) {
+
+}]);

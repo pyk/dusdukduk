71b3276bc983581558054ee3d017f4020b820f9b
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-09-16 02:01:05 +0700

    add checkbox for anti rayap & anti air, improve simple ux

--- src/app/controller/Product.controller.js
+++ src/app/controller/Product.controller.js
@@ -1,6 +1,34 @@
-app.controller("ProductCtrl", ["$scope",
-    function($s) {
+app.controller("ProductCtrl", ["$scope", "Chair",
+    function($s, Chair) {
 
+        // initalize default chair
+        $s.chair = new Chair(600000);
+
+        // mode changer
+        $s.mode = function(mode) {
+            if(mode == "classic") {
+                $s.chair.custom.status = false;
+                $s.chair.custom.harga = 0;
+            } else {
+                $s.chair.custom.status = true;
+                $s.chair.custom.harga = 10000;
+            }
+        };
+
+        // formater
+        $s.changeToRupiah = function(angka){
+            var rev     = parseInt(angka, 10).toString().split("").reverse().join("");
+            var rev2    = "";
+            for(var i = 0; i < rev.length; i++){
+                rev2  += rev[i];
+                if((i + 1) % 3 === 0 && i !== (rev.length - 1)){
+                    rev2 += ".";
+                }
+            }
+            return "Rp. " + rev2.split("").reverse().join("") + ",00";
+        };
+
+        // Color changer
         // Box bool
         $s.frontBox1Red = false;
         $s.frontBox2Red = false;

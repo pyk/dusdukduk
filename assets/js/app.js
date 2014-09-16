26d09bce2e46652912504c840811dfa289cf153c
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-09-17 04:04:30 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,42 +15,6 @@ app.config(["$routeProvider", "$locationProvider",
         redirectTo: "/"
     });
 }]);
-app.factory("Chair", function() {
-
-        // create class
-        var Chair = function(harga) {
-            this.custom = {
-                status: false,
-                harga: 0
-            };
-            // colorsnya sesuai namanya
-            // contoh: "red" atau "green"
-            // untuk scopenya nanti ditaruh image src
-            this.colors = {
-                frontBox1: null,
-                frontBox2: null,
-                frontBox3: null,
-                frontBox4: null,
-                frontBox5: null,
-                sideBox1: null,
-            };
-            this.utilitas = {
-                antiRayap: {
-                    harga: 0
-                },
-                antiAir: {
-                    harga: 0
-                }
-            };
-            this.hargaAwal = harga;
-        };
-
-        Chair.prototype.getHargaTotal = function() {
-            return this.hargaAwal + this.custom.harga + parseInt(this.utilitas.antiRayap.harga) + parseInt(this.utilitas.antiAir.harga);
-        };
-
-        return Chair;
-});
 app.controller("ListOfProductsCtrl", ["$scope",
     function($s) {
 
@@ -129,3 +93,39 @@ app.controller("ProductCtrl", ["$scope", "Chair",
             $s["sideBox"+x+"Green"] = true;
         };
 }]);
+app.factory("Chair", function() {
+
+        // create class
+        var Chair = function(harga) {
+            this.custom = {
+                status: false,
+                harga: 0
+            };
+            // colorsnya sesuai namanya
+            // contoh: "red" atau "green"
+            // untuk scopenya nanti ditaruh image src
+            this.colors = {
+                frontBox1: null,
+                frontBox2: null,
+                frontBox3: null,
+                frontBox4: null,
+                frontBox5: null,
+                sideBox1: null,
+            };
+            this.utilitas = {
+                antiRayap: {
+                    harga: 0
+                },
+                antiAir: {
+                    harga: 0
+                }
+            };
+            this.hargaAwal = harga;
+        };
+
+        Chair.prototype.getHargaTotal = function() {
+            return this.hargaAwal + this.custom.harga + parseInt(this.utilitas.antiRayap.harga) + parseInt(this.utilitas.antiAir.harga);
+        };
+
+        return Chair;
+});
